d720cdce1903255283c4f80e671037879d2393d8
Angela Liu <angelaliu@CMU-845857.WV.CC.CMU.EDU>
2015-05-02 15:19:33 -0400

    added custom homepages for baker and shipper

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
   include BreadExpressHelpers::Baking
 
   def home
-
-
+      @items_to_bake = OrderItem.unshipped.paginate(:page => params[:page]).per_page(10)   
+      @unshipped_orders = Order.not_shipped.paginate(:page => params[:page]).per_page(10) 
   end
-
+    
   def about
   end
 
